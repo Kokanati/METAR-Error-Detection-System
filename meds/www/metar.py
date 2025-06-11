0a10a3803f2981dcf226290765e3a18b26a3dcd7
Reginald <reggy.hingano@gmail.com>
2025-06-11 15:22:25 +1300

    Update redirect on login

--- meds/www/metar.py
+++ meds/www/metar.py
@@ -3,7 +3,7 @@ from frappe.utils.jinja import get_jenv
 
 def get_context(context):
     if frappe.session.user == "Guest":
-        frappe.local.flags.redirect_location = "/login"
+        frappe.local.flags.redirect_location = "/login?"redirect-to=/metar"
         raise frappe.Redirect
     return context
 
